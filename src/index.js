23751daa088d50d55528197373f897f31b0ef49f
phong47047 <phong47047@donga.edu.vn>
2022-05-18 15:52:47 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react' // nạp thư viện react
+import ReactDOM from 'react-dom/client' // nạp thư viện react-dom
+
+// Tạo component App
+function App() {
+    return (
+        <div>
+            <h1>Xin chào anh em F8!</h1>
+            <h2>Anh em ơi</h2>
+        </div>
+    )
+}
+
+// Render component App vào #root element
+//react@17
+// ReactDOM.render(<App />, document.getElementById('root'))
+
+//React@18
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
